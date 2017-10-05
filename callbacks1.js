0dfc7b8949fd8b7242999425a868e19ad553e400
Chad Yehia <lex.yehia@icloud.com>
2017-10-05 10:41:47 -0400

    Refactored to use Array.prototype.forEach instead

--- callbacks1.js
+++ callbacks1.js
@@ -1,15 +1,7 @@
 // The second argument/parameter is expected to be a function
-function findWaldo(arr, found) {
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i] === "Waldo") {
-            found(i);   // execute callback
-        }
-    }
-}
 
-function actionWhenFound(index) {
-    console.log(`Found Waldo at index ${index}.`);
-}
+["Alice", "Bob", "Waldo", "Winston"].forEach((element, index) => {
+    if (element === "Waldo") { console.log(`Found Waldo at index ${index}.`); }
+});
 
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
 
